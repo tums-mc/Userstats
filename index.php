d96939fb912752693b41f35fa045d9d255d7c79f
tums-mc <tums.server@gmail.com>
2013-09-16 02:21:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,24 +5,13 @@
 
 <body>
 
-
-
-
-
-
-
-
-
-
-
 <h1>Schau deine Spiel Statistik an!</h1>
 
 
-            <form action="stats.php" method="post" name="vip">
+<form action="stats.php" method="post" name="vip">
    <input type="text" name="username" placeholder="Spielername" required />
-   <input type="submit" content="Kaufen">
+   <input type="submit" content="anschauen">
 </form>
-<br />
 
 
 </body>
