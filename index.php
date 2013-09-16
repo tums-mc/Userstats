2293673977ff2d3ad3b4b5b5ce982b64a151bc79
tums-mc <tums.server@gmail.com>
2013-09-16 02:14:47 +0200

    Testseite für die User Statistiken
    stats.php zeigt die Daten aus den Reihen 1,2,3,... nicht an. Warum? :(
    
    User gibt seinen Namen auf index.php ein (nacher auf der Website) und
    gelangt auf stats.php.
    Dort soll dann der im Formular eingegebene Name die Tabelle Filtern und
    alle Daten für den User ausgeben.
    SELECT * FROM userstats WHERE username = $username
    
    userstats ist die Tabelle
    die hat 174 Spalten
    1x username und dann die Zahlen 1-173 (Es gibt 173 Blöcke in MC)
    Max's Plugin schreibt in die Datenbank wenn ein user einen Block abbaut.
    Also z.b. mrheat baut 2 steine ab dann kommt in spalte 1 "mrheat" und in
    Spalte 1 (Stein hat die ID 1) "2"
    
    Hoffe du kannst mir weiterhelfen :/

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+</head>
+
+<body>
+
+
+
+
+
+
+
+
+
+
+
+<h1>Schau deine Spiel Statistik an!</h1>
+
+
+            <form action="stats.php" method="post" name="vip">
+   <input type="text" name="username" placeholder="Spielername" required />
+   <input type="submit" content="Kaufen">
+</form>
+<br />
+
+
+</body>
+</html>
---
+++ stats.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+
+<html lang="de">
+<head>
+	<title>Deine Statistik</title>
+</head>
+<body>
+
+
+
+<?php
+$db = mysql_connect("localhost","ni12347_3sql13","74937493") or
+  die ("Verbindung fehlgeschlagen");
+
+mysql_select_db("ni12347_3sql13",$db) or die("Konnte die Datenbank nicht waehlen.");
+
+$username = $_POST[username];  //Spielername
+
+mysql_query("SELECT * FROM userstats WHERE username = $username"); 
+
+
+?>
+
+              Hallo <? echo $username?>    <br />
+
+              Hier sind ein paar Statistiken von dir:
+
+              <table>
+              	<tr>
+              		<th>(Blockbild)</th>
+              		<th>Blockname</th>
+              		<th>ID</th>
+              		<th>abgebaut</th>
+              		<th>gesetzt</th>
+              	</tr>
+
+              	<tr>
+            		<td>...</td>
+            		<td>Stein</td>
+            		<td>1</td>
+            		<td><? echo "". $row['1']?></td>
+            		<td>...</td>
+            	</tr>
+            	<tr>
+            		<td>...</td>
+            		<td>usw</td>
+            		<td>usw</td>
+            		<td><? echo "". $row['2']?></td>
+            		<td>...</td>
+            	</tr>
+            	<tr>
+            		<td>...</td>
+            		<td>usw</td>
+            		<td>usw</td>
+            		<td><? echo "". $row['3']?></td>
+            		<td>...</td>
+            	</tr>
+            	<tr>
+            		<td>...</td>
+            		<td>usw</td>
+            		<td>usw</td>
+            		<td><? echo "". $row['4']?></td>
+            		<td>...</td>
+            	</tr>
+            	<tr>
+            		<td>...</td>
+            		<td>usw</td>
+            		<td>usw</td>
+            		<td><? echo "". $row['5']?></td>
+            		<td>...</td>
+            	</tr>
+            	<tr>
+            		<td>...</td>
+            		<td>usw</td>
+            		<td>usw</td>
+            		<td><? echo "". $row['6']?></td>
+            		<td>...</td>
+            	</tr>
+
+
+
+
+
+</body>
+</html>
